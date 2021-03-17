5f381c2a593ae146dc6bf5750fd4365d6395a73a
muhuahaha <supercodekim@gmail.com>
2021-03-17 05:38:26 +0100

    random user test vue

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+const app = Vue.createApp({
+    data() {
+      return {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'john@gmail.com',
+        gender: 'male',
+        picture: 'assets/img/ytk.png',
+      }
+    },
+    methods: {
+      async getUser() {
+        const res = await fetch('https://randomuser.me/api')
+        const { results } = await res.json()
+
+        console.log(results)
+
+        this.firstName = results[0].name.first
+        this.lastName = results[0].name.last
+        this.email = results[0].email
+        this.gender = results[0].gender
+        this.picture = results[0].picture.large
+      },
+    },
+  })
+
+  app.mount('#app')
